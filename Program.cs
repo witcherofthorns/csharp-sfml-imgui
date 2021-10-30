32cefcf462fd36aa573078ea68aa2f57261a7e30
Miron Badurow <sabbat50kproducer@gmail.com>
2021-10-30 19:12:11 +0300

    init commit

---
+++ Program.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using ImGuiGL;
+using ImGuiNET;
+using OpenTK.Graphics.OpenGL;
+using OpenTK.Mathematics;
+using OpenTK.Windowing.Desktop;
+using SFML.Graphics;
+using SFML.System;
+using SFML.Window;
+
+namespace Game
+{
+    class Program
+    {
+        static private GameWindow glWindow;
+        static private RenderWindow renderWindow;
+        static private ContextSettings renderWindowContext;
+        static private ImGuiController imgui;
+
+        static private Clock clock;
+        static private Time timePerTick = Time.Zero;
+        static private Time timeSinceLastUpdate = Time.Zero;
+        static private Time frameTime = Time.Zero;
+
+        static void Main(string[] args)
+        {
+            GameWindowSettings glWindowSettings = new GameWindowSettings();
+            NativeWindowSettings glNativeWindowSettings = new NativeWindowSettings();
+            glNativeWindowSettings.StartVisible = false;
+            glWindow = new GameWindow(glWindowSettings, glNativeWindowSettings);
+
+            renderWindowContext = new ContextSettings();
+            renderWindowContext.AttributeFlags = ContextSettings.Attribute.Default;
+            renderWindow = new RenderWindow(new VideoMode(800, 600), "C# SFML + OPENGL", Styles.Default, renderWindowContext);
+
+            renderWindow.Resized += RenderWindow_Resized;
+            renderWindow.Closed += RenderWindow_Closed;
+            renderWindow.TextEntered += RenderWindow_TextEntered;
+            renderWindow.KeyPressed += RenderWindow_KeyPressed;
+            renderWindow.MouseWheelScrolled += RenderWindow_MouseWheelScrolled;
+
+            glWindow.Size = new OpenTK.Mathematics.Vector2i((int)renderWindow.Size.X, (int)renderWindow.Size.Y);
+            glWindow.Location = new OpenTK.Mathematics.Vector2i(renderWindow.Position.X, renderWindow.Position.Y);
+            imgui = new ImGuiController((int)renderWindow.Size.X, (int)renderWindow.Size.Y);
+
+            renderWindow.SetActive(false);
+            CreateFixedTimeFrame();
+
+
+            while (renderWindow.IsOpen)
+            {
+                renderWindow.DispatchEvents();
+
+                frameTime = clock.Restart();
+                timeSinceLastUpdate += frameTime;
+                imgui.Update(ref renderWindow, frameTime.AsSeconds());
+
+                while (timeSinceLastUpdate > timePerTick)
+                {
+                    timeSinceLastUpdate -= timePerTick;
+                    // fixed update
+                }
+
+                // draw
+                renderWindow.SetActive(true);
+                GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit | ClearBufferMask.StencilBufferBit);
+                ImGui.ShowDemoWindow();
+                imgui.Render();
+                glWindow.SwapBuffers();
+                renderWindow.Display();
+            }
+        }
+
+        private static void CreateFixedTimeFrame()
+        {
+            clock = new Clock();
+            timePerTick = Time.FromSeconds(1.0f / 60.0f);
+            timeSinceLastUpdate = Time.Zero;
+            frameTime = Time.Zero;
+            timeSinceLastUpdate += clock.Restart();
+        }
+
+        private static void RenderWindow_KeyPressed(object sender, KeyEventArgs e)
+        {
+            // ...
+        }
+        private static void RenderWindow_TextEntered(object sender, TextEventArgs e)
+        {
+            imgui.PressChar(e.Unicode[0]);
+        }
+        private static void RenderWindow_MouseWheelScrolled(object sender, MouseWheelScrollEventArgs e)
+        {
+            imgui.MouseScroll(e.X, e.Y, e.Delta);
+        }
+        private static void RenderWindow_Closed(object sender, EventArgs e)
+        {
+            renderWindow.Close();
+        }
+        private static void RenderWindow_Resized(object sender, SizeEventArgs e)
+        {
+            GL.Viewport(0, 0, (int)e.Width, (int)e.Height);
+            imgui.WindowResized((int)e.Width, (int)e.Height);
+        }
+    }
+}
---
+++ libs/imgui/GLWindow.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ImGuiNET;
+using System.Drawing;
+using OpenTK.Graphics.OpenGL4;
+using OpenTK.Mathematics;
+using OpenTK.Windowing.Common;
+using OpenTK.Windowing.Desktop;
+using System.Diagnostics;
+
+namespace ImGuiGL
+{
+    public class GLWindow : GameWindow
+    {
+        ImGuiController imgui;
+
+
+        /*
+        public GLWindow() : base(GameWindowSettings.Default, new NativeWindowSettings(){ Size = new Vector2i(1600, 900), APIVersion = new Version(4, 5) })
+        {
+
+        }
+        */
+
+        public GLWindow() : base(GameWindowSettings.Default, new NativeWindowSettings() { Size = new Vector2i(800, 600), APIVersion = new Version(4, 5) })
+        {
+
+        }
+
+
+
+        protected override void OnLoad()
+        {
+            base.OnLoad();
+
+            Title += ": OpenGL Version: " + GL.GetString(StringName.Version);
+
+            imgui = new ImGuiController(ClientSize.X, ClientSize.Y);
+        }
+        
+        protected override void OnResize(ResizeEventArgs e)
+        {
+            base.OnResize(e);
+
+            // Update the opengl viewport
+            GL.Viewport(0, 0, ClientSize.X, ClientSize.Y);
+
+            // Tell ImGui of the new size
+            imgui.WindowResized(ClientSize.X, ClientSize.Y);
+        }
+
+        protected override void OnRenderFrame(FrameEventArgs e)
+        {
+            base.OnRenderFrame(e);
+
+            imgui.Update(this, (float)e.Time);
+
+            GL.ClearColor(new Color4(0, 32, 48, 255));
+            GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit | ClearBufferMask.StencilBufferBit);
+
+
+            if(ImGuiNET.ImGui.Begin("test"))
+            {
+                if(ImGuiNET.ImGui.Button("click me"))
+                {
+
+                }
+            }
+            ImGuiNET.ImGui.End();
+
+            //ImGui.ShowDemoWindow();
+
+            imgui.Render();
+
+            GLImGuiUtil.CheckGLError("End of frame");
+
+            SwapBuffers();
+        }
+
+        protected override void OnTextInput(TextInputEventArgs e)
+        {
+            base.OnTextInput(e);
+            imgui.PressChar((char)e.Unicode);
+        }
+
+        protected override void OnMouseWheel(MouseWheelEventArgs e)
+        {
+            base.OnMouseWheel(e);
+            
+            imgui.MouseScroll(e.Offset);
+        }
+    }
+}
